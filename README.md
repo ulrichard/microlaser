145dd377e2123f0a041a7c66e90624dca3be9c52
Richard Ulrich <Richard.Ulrich@cubx-software.com>
2013-02-03 17:59:33 +0100

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ microlaser
 ==========
 
 minimalist laser show using a CD-ROM lens mechanism
+
+see also : 
+http://blog.ulrichard.ch/?p=774
+http://ulrichard.ch/gallery2/main.php?g2_itemId=7349
+
---
+++ microlaser.ino
@@ -0,0 +1,86 @@
+// This program is free software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation; either version 2 of the License, or (at your option) any later
+// version.
+
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+// This program is a minimalist laser show 
+// Created by Richard Ulrich <richi@paraeasy.ch>
+// Inspired by : 
+// http://www.instructables.com/id/Micro-LASER-Show-with-a-CD-Lens-Mechanism/#intro
+// http://spt06.chez-alice.fr/00/scan1.htm
+// http://luckylarry.co.uk/arduino-projects/control-a-dc-motor-with-arduino-and-l293d-chip/
+// http://letsmakerobots.com/node/2074
+// http://www.atmel.com/devices/atmega8.aspx
+
+const uint8_t laserPin     =  8;
+const uint8_t enablePin    =  7;   // pin 1 on L293D
+const uint8_t motorXPinPos = 10;   // pin 2 on L293D
+const uint8_t motorXPinNeg = 11;   // pin 7 on L293D
+const uint8_t motorYPinPos =  6;   // pin 10 on L293D
+const uint8_t motorYPinNeg =  9;   // pin 15 on L293D
+const uint8_t steps = 50;
+const uint8_t pause = 0;
+const float scaleX = 0.5;
+const float scaleY = 1;
+
+const int8_t offsetX = 0;
+const int8_t offsetY = 120; // to compensate the missing 4th PWM of the AtMega8
+const float scaleXPos = scaleX;
+const float scaleXNeg = scaleX;
+const float scaleYPos = scaleY;
+const float scaleYNeg = 0; // scaleY; -> as the atmega 8 has only 3 PWM channels, this one doesn't work anyway
+
+void setup()
+{
+    // set all the other pins you're using as outputs:
+    pinMode(laserPin,     OUTPUT);
+    pinMode(enablePin,    OUTPUT);
+    pinMode(motorXPinPos, OUTPUT);
+    pinMode(motorXPinNeg, OUTPUT);
+    pinMode(motorYPinPos, OUTPUT);
+    pinMode(motorYPinNeg, OUTPUT);
+
+    digitalWrite(enablePin, HIGH);
+    digitalWrite(laserPin,  HIGH);
+}
+
+float angle = 0;
+
+void loop()
+{    
+    const float x = sin(angle);
+    float y = cos(angle);
+
+	if(angle < 45.0 || angle > 370)
+		y = 0.75 - (y - 0.75);
+    
+    if(x >= 0)
+    {
+        digitalWrite(motorXPinPos, LOW);
+        analogWrite(motorXPinNeg, x * scaleXPos * 255);
+    }
+    else
+    {
+        analogWrite(motorXPinPos, -x * scaleXNeg * 255);
+        digitalWrite(motorXPinNeg, LOW);
+    }
+    if(y >= 0)
+    {
+        digitalWrite(motorYPinPos, LOW);
+        analogWrite(motorYPinNeg, y * scaleYPos * 255);
+    }
+    else
+    {
+        analogWrite(motorYPinPos, -y * scaleYNeg * 255);
+        digitalWrite(motorYPinNeg, LOW);
+    }
+  
+    angle += 2 * PI / steps;
+    if(angle > 2 * PI)
+        angle = 0.0;
+    delay(pause);
+}
